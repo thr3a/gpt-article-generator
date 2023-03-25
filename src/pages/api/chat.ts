30a4e0d2ada41b230fd11a7ecfcc4c1adae1bf07
thr3a <thr3a@example.com>
2023-03-26 01:42:08 +0900

    fix:

--- src/pages/api/chat.ts
+++ src/pages/api/chat.ts
@@ -20,6 +20,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       openAIApiKey: process.env.OPENAI_APIKEY,
       modelName: 'gpt-3.5-turbo',
       streaming: true,
+      temperature: 0.7,
       callbackManager: CallbackManager.fromHandlers({
         async handleLLMNewToken(token: string) {
           res.write(`${token}`);
