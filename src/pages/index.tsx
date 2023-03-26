8ccfa036327b22ae9b676d1ad15c7684c37c3d77
thr3a <thr3a@example.com>
2023-03-26 20:18:03 +0900

    fix:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ export const getServerSideProps: GetServerSideProps = async ({ res }) => {
 const IndexPage: NextPage<Props> = ({ csrfToken }) => {
   return (
     <>
-      <Text fz="sm" mb="md">タイトルと箇条書きから文章を生成します。</Text>
+      <Text fz="sm" mb="md">箇条書きから文章を生成します。</Text>
       <TaskForm csrfToken={csrfToken}></TaskForm>
     </>
   );
