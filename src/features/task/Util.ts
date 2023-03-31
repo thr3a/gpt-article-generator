00f1e7ca5c8314663ef39b01f31b47edc13a0082
thr3a <thr3a@example.com>
2023-04-01 02:46:47 +0900

    update:

--- src/features/task/Util.ts
+++ src/features/task/Util.ts
@@ -3,7 +3,7 @@ import { TaskProps } from '@/features/task/FormContext';
 export const assistantPrompt = (value: TaskProps): string => {
   return `
 [Order]
-Convert the bullet point script into a article written in Japanese without omitting the original information.
+Convert the bullet point script into a article written in fluent Japanese without omitting the original information.
 
 [Rule]
 - 話し言葉
